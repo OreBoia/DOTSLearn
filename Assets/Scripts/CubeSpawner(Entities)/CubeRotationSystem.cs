b72a202e73e589c261a252fc44a3a81d67c5f9ad
Edoardo Del Sarto <e99delsarto@gmail.com>
2025-09-12 12:18:09 +0200

    - Created tutorial to Entities, Authoring and Sytema (CubeGenerator)
    - Created new Unity scene and meta files for the tank tutorial.

---
+++ Assets/Scripts/CubeSpawner(Entities)/CubeRotationSystem.cs
@@ -0,0 +1,23 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public partial struct CubeRotationSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        var deltaTime = SystemAPI.Time.DeltaTime;
+
+        // This foreach loops through all entities that have LocalTransform and RotationSpeed components. 
+        // You need to modify the LocalTransform, so it is wrapped in RefRW (read-write). 
+        // You only need to read the RotationSpeed, so it is wrapped in RefRO (read only). 
+        foreach (var (trasform, rotationSpeed) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotationSpeed>>())
+        {
+            // Rotate the transform around the Y axis. 
+            var radians = rotationSpeed.ValueRO.RadiansPerSeconds * deltaTime;
+            trasform.ValueRW = trasform.ValueRW.RotateY(radians);
+        }
+    }
+}
---
+++ Assets/Scripts/CubeSpawner(Entities)/SpawnerAuthoring.cs
@@ -0,0 +1,26 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class SpawnerAuthoring : MonoBehaviour
+{
+    public GameObject CubePrefab;
+
+    class Baker : Baker<SpawnerAuthoring>
+    {
+        public override void Bake(SpawnerAuthoring authoring)
+        {
+            var entity = GetEntity(authoring, TransformUsageFlags.None);
+            var spawner = new SpawnerComponentData
+            {
+                Prefab = GetEntity(authoring.CubePrefab, TransformUsageFlags.Dynamic)
+            };
+
+            AddComponent(entity, spawner);
+        }
+    }
+}
+
+struct SpawnerComponentData : IComponentData
+{
+    public Entity Prefab;
+}
